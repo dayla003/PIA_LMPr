80a53a37036719fd49c5b8ac2cd1e1ab4b6882e4
dayla003 <dayla.carrizalesortg@uanl.edu.mx>
2025-05-18 22:25:10 -0600

    Mostrar datos de la bd

--- mi_peliculas/peliculas/views.py
+++ mi_peliculas/peliculas/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Pelicula
 
 # Create your views here.
 def inicio(request):
     return render(request, 'paginas/inicio.html')
 
 def peliculas(request):
-    return render(request, 'peliculas/index.html')
+    peliculas = Pelicula.objects.all()
+    return render(request, 'peliculas/index.html', {'peliculas': peliculas})
